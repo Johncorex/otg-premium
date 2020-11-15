443db2fc01867ec2df863c74de8e666dcfa1b169
Johncore <32520764+Johncorex@users.noreply.github.com>
2020-11-15 14:52:39 -0300

    remove custom

--- data/scripts/globalevents/others/startup.lua
+++ data/scripts/globalevents/others/startup.lua
@@ -52,10 +52,6 @@ function serverstartup.onStartup()
 	print("> Loaded all actions in the map")
 	print("> Loaded all uniques in the map")
 
-	print(">> Loading custom maps")
-	-- Custom maps table
-	loadCustomMaps()
-
 	for i = 1, #startupGlobalStorages do
 		Game.setStorageValue(startupGlobalStorages[i], 0)
 	end
