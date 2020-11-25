c6d3830865291df1ed806e29780de48962275abf
Xiolones <Xiolones@users.noreply.github.com>
2020-11-25 03:19:54 -0300

    Cyclopedia -> Characters

--- src/protocolgame.h
+++ src/protocolgame.h
@@ -122,7 +122,7 @@ class ProtocolGame final : public Protocol
         void parseCyclopediaCharacterInfo(NetworkMessage& msg);
 		void parseHighscores(NetworkMessage& msg);
 	    void sendHighscoresNoData();
-	    void sendHighscores(std::vector<HighscoreCharacter>& characters, uint8_t categoryId, uint32_t vocationId, uint16_t page, uint16_t pages);
+	  	void sendHighscores(const std::vector<HighscoreCharacter>& characters, uint8_t categoryId, uint32_t vocationId, uint16_t page, uint16_t pages);
 	  
         void parseTournamentLeaderboard(NetworkMessage& msg);
 
@@ -250,11 +250,12 @@ class ProtocolGame final : public Protocol
     
     void sendTournamentLeaderboard();
 
+    void sendCyclopediaCharacterNoData(CyclopediaCharacterInfoType_t characterInfoType, uint8_t errorCode);
 		void sendCyclopediaCharacterBaseInformation();
 		void sendCyclopediaCharacterGeneralStats();
 		void sendCyclopediaCharacterCombatStats();
-		void sendCyclopediaCharacterRecentDeaths();
-		void sendCyclopediaCharacterRecentPvPKills();
+		void sendCyclopediaCharacterRecentDeaths(uint16_t page, uint16_t pages, const std::vector<RecentDeathEntry>& entries);
+		void sendCyclopediaCharacterRecentPvPKills(uint16_t page, uint16_t pages, const std::vector<RecentPvPKillEntry>& entries);
 		void sendCyclopediaCharacterAchievements();
 		void sendCyclopediaCharacterItemSummary();
 		void sendCyclopediaCharacterOutfitsMounts();
